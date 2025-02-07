3d008ceda9d6aa370ec4e7976b2d2324d7807bdb
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2025-02-07 15:08:36 -0600

    Add project files.

---
+++ Formulario/Controllers/FormularioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Formulario.Models;
+
+namespace Formulario.Controllers
+{
+    public class FormularioController : Controller
+    {
+        // GET: Formulario/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(FormularioModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return View("Validacion", model);
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Confirmacion(FormularioModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Formulario/Models/FormularioModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formulario.Models
+{
+    public class FormularioModel
+    {
+        [Required(ErrorMessage = "El espacio debe tener un nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+        public string CorreoElectronico { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio.")]
+        public string Mensaje { get; set; }
+    }
+}
